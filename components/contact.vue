cf4c8267dd1fb4d12959c833e51734b20b818a9d
Joshua Fry <fry.developer@gmail.com>
2021-02-07 13:06:55 -0700

    Adding netlify attr to form

--- components/contact.vue
+++ components/contact.vue
@@ -5,8 +5,9 @@
   >
     <v-card
       width="600"
+      class=""
     >
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST" data-netlify="true" netlify>
         <p>
           <label>Your Name: <input type="text" name="name" /></label>
         </p>
@@ -14,9 +15,9 @@
           <label>Your Email: <input type="email" name="email" /></label>
         </p>
         <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
+          <label>Subject: <select name="subject[]" multiple>
+            <option value="order paddle">Order Paddle</option>
+            <option value="inquiry">General Inquiry</option>
           </select></label>
         </p>
         <p>
