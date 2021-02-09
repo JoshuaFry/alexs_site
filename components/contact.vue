db8771614a11ae718d4901befce87ef6fd53f397
Joshua Fry <fry.developer@gmail.com>
2021-02-09 09:53:56 -0700

    ...

--- components/contact.vue
+++ components/contact.vue
@@ -1,96 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <h1>{{ teaName }}</h1>
-    </div>
-    <div>
-      <form
-        name="vue-tea"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="vue-tea" />
-        <div class="tea">
-          <h2>Pick a Tea</h2>
-          <label
-            v-for="tea in teaNames"
-            :key="tea"
-            :class="{
-              'tea-label': true,
-              checked: tea === chosenTea
-            }"
-          >
-            <input
-              id="tea"
-              v-model="chosenTea"
-              name="tea"
-              type="radio"
-              :value="tea"
-            />
-            <span>{{ tea }}</span>
-          </label>
-        </div>
-        <button>Submit</button>
-      </form>
-    </div>
-  </div>
+  <form
+    name="contact"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        type="radio"
+        name="panelist"
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
 </template>
-
 <script>
     export default {
-        data() {
-            return {
-                chosenTea: 'Earl Grey',
-                teaTypes: {
-                    'Earl Grey': 'London',
-                    'Irish Breakfast': 'Dublin',
-                    Chai: 'Bombay',
-                    Rose: 'Atlantic City',
-                    Matcha: 'Tokyo',
-                    Rooibos: 'Cape Town',
-                    'Yerba Mate': 'Montreal',
-                    Green: 'Oregon Mist'
-                }
+        name: "QAForm",
+        methods: {
+            updatePanelist (ev) {
+                this.currentPanelist = ev.target.value
             }
         },
-        computed: {
-            teaName() {
-                return `${this.teaTypes[this.chosenTea]} Fog`
-            },
-            teaNames() {
-                return Object.keys(this.teaTypes)
+        data () {
+            return {
+                panelists: ['Evan You', 'Chris Fritz'],
+                currentPanelist: 'Evan You'
             }
         }
     }
 </script>
-
-<style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-  .links {
-    padding-top: 15px;
-  }
-</style>
