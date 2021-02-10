da886fd882cb87151511fa7fae5b0936742668b4
Joshua Fry <fry.developer@gmail.com>
2021-02-10 14:44:26 -0700

    testing Netlify form with vuetify form

--- components/contact.vue
+++ components/contact.vue
@@ -1,24 +1,118 @@
 <template>
-  <form
-    name="contact"
-    action="/"
-    method="post"
-    netlify
-    data-netlify-honeypot="bot-field"
+  <v-col
+    cols="12"
+    class="flex-center"
   >
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit" value="Send message">Send</button>
-    </p>
-  </form>
+    <v-card
+      width="600"
+    >
+      <form
+
+      >
+        <v-text-field
+          v-model="name"
+          :error-messages="nameErrors"
+          :counter="10"
+          label="Name"
+          required
+          name="name"
+          @input="$v.name.$touch()"
+          @blur="$v.name.$touch()"
+        ></v-text-field>
+        <v-select
+          v-model="select"
+          :items="items"
+          :error-messages="selectErrors"
+          label="Subject"
+          required
+          @change="$v.select.$touch()"
+          @blur="$v.select.$touch()"
+          name="subject"
+        ></v-select>
+        <v-text-field
+          v-model="email"
+          :error-messages="emailErrors"
+          label="E-mail"
+          required
+          name="email"
+          @input="$v.email.$touch()"
+          @blur="$v.email.$touch()"
+        ></v-text-field>
+        <v-textarea
+          v-model="content"
+          :error-messages="contentErrors"
+          label="Message"
+          required
+          name="message"
+          @input="$v.name.$touch()"
+          @blur="$v.name.$touch()"
+        ></v-textarea>
+        <v-btn
+          @click="submit"
+          class="flex-center"
+        >
+          submit
+        </v-btn>
+      </form>
+    </v-card>
+  </v-col>
 </template>
 
+<script>
+    import {validationMixin} from 'vuelidate'
+    import {required, maxLength, email} from 'vuelidate/lib/validators'
+    export default {
+        mixins: [validationMixin],
+        validations: {
+            name: {required, maxLength: maxLength(10)},
+            select: { required },
+            email: {required, email},
+            content: {required, maxLength: maxLength(500)},
+        },
+        name: 'contact',
+        data: () => ({
+            name: '',
+            email: '',
+            subject: '',
+            content: '',
+            select: null,
+            items: [
+                'General Inquiry',
+                'Paddle Order',
+            ],
+        }),
+        computed: {
+            nameErrors() {
+                const errors = []
+                if (!this.$v.name.$dirty) return errors
+                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+                !this.$v.name.required && errors.push('Name is required.')
+                return errors
+            },
+            emailErrors() {
+                const errors = []
+                if (!this.$v.email.$dirty) return errors
+                !this.$v.email.email && errors.push('Must be valid e-mail')
+                !this.$v.email.required && errors.push('E-mail is required')
+                return errors
+            },
+            selectErrors () {
+                const errors = []
+                if (!this.$v.select.$dirty) return errors
+                !this.$v.select.required && errors.push('Item is required')
+                return errors
+            },
+            contentErrors() {
+                const errors = []
+                if (!this.$v.name.$dirty) return errors
+                !this.$v.subject.maxLength && errors.push('Content must be at most 500 characters long')
+                return errors
+            },
+        },
+        methods: {
+            submit() {
+                this.$v.$touch()
+            },
+        },
+    }
+</script>
