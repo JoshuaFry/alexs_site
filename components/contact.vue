6a21cc557ba6376c9419c8ca49851f63aa87bab3
Joshua Fry <fry.developer@gmail.com>
2021-02-10 16:12:45 -0700

    Converting back to regular HTML form

--- components/contact.vue
+++ components/contact.vue
@@ -1,117 +1,32 @@
 <template>
-  <v-col
-    cols="12"
-    class="flex-center"
+  <form
+    name="contact"
+    action="/"
+    method="post"
+    netlify
+    data-netlify-honeypot="bot-field"
   >
-    <v-card
-      width="600"
-    >
-      <form
-        name="contact"
-        netlify
-        method="post"
-        action="/"
-      >
-        <v-text-field
-          v-model="name"
-          :error-messages="nameErrors"
-          :counter="30"
-          label="Name"
-          name="name"
-          required
-          @change="$v.name.$touch()"
-          @blur="$v.name.$touch()"
-        ></v-text-field>
-        <v-select
-          v-model="subject"
-          :items="items"
-          :error-messages="subjectErrors"
-          label="Subject"
-          required
-          @change="$v.subject.$touch()"
-          @blur="$v.subject.$touch()"
-          name="subject"
-        ></v-select>
-        <v-text-field
-          v-model="email"
-          :error-messages="emailErrors"
-          label="E-mail"
-          required
-          @change="$v.email.$touch()"
-          @blur="$v.email.$touch()"
-          name="email"
-        ></v-text-field>
-        <v-textarea
-          v-model="content"
-          :counter="500"
-          :error-messages="contentErrors"
-          label="Message"
-          required
-          @input="$v.content.$touch()"
-          @blur="$v.content.$touch()"
-          name="message"
-        ></v-textarea>
-        <button type="submit" value="Send message">Send</button>
-      </form>
-    </v-card>
-  </v-col>
+    <input type="hidden" name="form-name" value="contact"/>
+    <p>
+      <label>Your Name: <input type="text" name="name" required/></label>
+    </p>
+    <p>
+      <label for="subject">Subject:</label>
+
+      <select name="subject" id="subject" required>
+        <option value="general inquiry">General Inquiry</option>
+        <option value="order a paddle">Order a Paddle</option>
+      </select>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" required/></label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message" required></textarea></label>
+    </p>
+    <p>
+      <button type="submit" value="Send message">Send</button>
+    </p>
+  </form>
 </template>
 
-<script>
-    import {validationMixin} from 'vuelidate'
-    import {required, maxLength, email} from 'vuelidate/lib/validators'
-    export default {
-        mixins: [validationMixin],
-        validations: {
-            name: {required, maxLength: maxLength(30)},
-            subject: { required },
-            email: {required, email},
-            content: {required, maxLength: maxLength(500)},
-        },
-        name: 'contact',
-        data: () => ({
-            name: '',
-            email: '',
-            subject: 'General Inquiry',
-            content: '',
-            items: [
-                'General Inquiry',
-                'Paddle Order',
-            ],
-        }),
-        computed: {
-            nameErrors() {
-                const errors = []
-                if (!this.$v.name.$dirty) return errors
-                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-                !this.$v.name.required && errors.push('Name is required.')
-                return errors
-            },
-            emailErrors() {
-                const errors = []
-                if (!this.$v.email.$dirty) return errors
-                !this.$v.email.email && errors.push('Must be valid e-mail')
-                !this.$v.email.required && errors.push('E-mail is required')
-                return errors
-            },
-            subjectErrors () {
-                const errors = []
-                if (!this.$v.subject.$dirty) return errors
-                !this.$v.subject.required && errors.push('Item is required')
-                return errors
-            },
-            contentErrors() {
-                const errors = []
-                if (!this.$v.content.$dirty) return errors
-                !this.$v.content.maxLength && errors.push('Content must be at most 500 characters long')
-                !this.$v.content.required && errors.push('Item is required')
-                return errors
-            },
-        },
-        methods: {
-            submit() {
-                this.$v.$touch()
-            },
-        },
-    }
-</script>
